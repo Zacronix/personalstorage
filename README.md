62da4d0a058a49a4886cec22d8a1c5c1ae46e0fa
Zacronix <j_halff@live.nl>
2017-11-15 13:37:36 +0100

    Editted some files v2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # personalstorage
+
+Website created by Jelmer Halff
+
+There is a log in system involved in this website,
+you can take a look at the design and code but don't mind it too much. :)
--- files.php
+++ files.php
@@ -7,21 +7,35 @@
   </head>
   <body>
 
+    <!-- navbar and searchbar start -->
+
     <div class="searchbar">
-      <p id="logo">COMPANY NAME</p>
+      <p id="logo">PERSONAL STORAGE //</p>
+      <p id="logoname">BY JELMER HALFF</p>
+      <a href="login.php"><p id="logout">Log Out</p></a>
     </div>
 
     <div class="navbar">
       <ul>
         <li><a href="files.php">Files</a></li>
-        <li><a href="recentfiles.php">Recent files</a></li>
         <li><a href="pictures.php">Pictures</a></li>
         <li><a href="links.php">Links</a></li>
         <li><a href="designs.php">Designs</a></li>
       </ul>
     </div>
 
-    <h1 id="title">All files</h1>
+    <!-- Navbar and searchbar end -->
+
+
+
+    <!-- Title start -->
+
+    <div class="titletext">
+      <h2>All files</h2>
+      <h3>You can find all files here, but they are not ordered.</h3>
+    </div>
+
+    <!-- title end -->
 
   </body>
 </html>
