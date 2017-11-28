ee2c85d99f84c2bdcc1843f7c4a959d48caaaf51
Zacronix <j_halff@live.nl>
2017-11-28 20:50:57 +0100

    files.php editted

--- files.php
+++ files.php
@@ -1,81 +1,78 @@
 <?php
-	session_start();
-    $username = $_SESSION['username'];
+session_start();
+$username = $_SESSION['username'];
 
-    if ($username === null) {
-        header("Location: index.php");
-    }
+if ($username === null) {
+    header("Location: index.php");
+}
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
 
     <link rel="stylesheet" href="/css/base/app.css">
     <link rel="stylesheet" href="/css/pages/files.css">
 
-		<script src="https://use.fontawesome.com/e900784a2c.js"></script>
+    <script src="https://use.fontawesome.com/e900784a2c.js"></script>
 
     <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed" rel="stylesheet">
 
     <title>My files</title>
-  </head>
-  <body>
+</head>
+<body>
 
-      <!-- Navbar start -->
-
-
-
-      <div class="navbar">
-        <h1 class="logo">PERSONAL STORAGE</h1>
-        <input type="text" placeholder="Search..." id="search">
-
-				<div class="dropdown">
-					<h3 class="user"><a href="#"><?php echo $username ?></a></h3>
-					<div class="dropdown-content">
-						<p><a href="index.php">LOG OUT</a></p>
-					</div>
-				</div>
-				
-      </div>
-
-      <div class="sidebar">
-				<ul>
-					<li><a href="files.php"><i class="fa fa-home" aria-hidden="true"></i></a></li>
-					<li><a href="links.php"><i class="fa fa-link" aria-hidden="true"></i></a></li>
-					<li><a href="upload.php"><i class="fa fa-upload" aria-hidden="true"></i></a></li>
-					<li><a href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a></li>
-				</ul>
-      </div>
+<!-- Navbar start -->
 
 
+<div class="navbar">
+    <h1 class="logo">PERSONAL STORAGE</h1>
+    <input type="text" placeholder="Search..." id="search">
 
-      <!-- Navbar end -->
-
-
-
-      <h2 class="title">Dashboard</h2>
-      <h3 class="subtitle">Statistics & Details</h3>
-
-      <div class="stats-container">
-        <div class="storagestats">
-          <h2>STORAGE</h2>
-          <b>0 bytes</b><h3 class="storagesubtitle">USED OUT OF 5GB</h3>
+    <div class="dropdown">
+        <h3 class="user"><a href="#"><?php echo $username ?></a></h3>
+        <div class="dropdown-content">
+            <p><a href="index.php">LOG OUT</a></p>
         </div>
+    </div>
 
-        <div class="otherstats">
-          <h2>OTHER STATS</h2>
-          <h3>These are some other statistics</h3>
-        </div>
-      </div>
+</div>
 
-      <div class="recentfiles">
-        <h2 class="recentfilestitle">RECENT FILES</h2>
-        <h3>No files uploaded yet...</h3>
-      </div>
+<div class="sidebar">
+    <ul>
+        <li><a href="files.php"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+        <li><a href="links.php"><i class="fa fa-link" aria-hidden="true"></i></a></li>
+        <li><a href="upload.php"><i class="fa fa-upload" aria-hidden="true"></i></a></li>
+        <li><a href="settings.php"><i class="fa fa-cog" aria-hidden="true"></i></a></li>
+    </ul>
+</div>
 
 
+<!-- Navbar end -->
 
-  </body>
+
+<h2 class="title">Dashboard</h2>
+<h3 class="subtitle">Statistics & Details</h3>
+
+<div class="stats-container">
+    <div class="storagestats">
+        <h2>STORAGE</h2>
+        <b>0 bytes</b>
+        <h3 class="storagesubtitle">USED OUT OF 5GB</h3>
+    </div>
+
+    <div class="otherstats">
+        <h2>OTHER STATS</h2>
+        <h3>These are some other statistics</h3>
+    </div>
+</div>
+
+<div class="recentfiles">
+    <h2 class="recentfilestitle">RECENT FILES</h2>
+    <h3>No files uploaded yet...</h3>
+</div>
+
+
+</body>
 </html>
